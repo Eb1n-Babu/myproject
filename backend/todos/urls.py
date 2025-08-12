4786604ef7f5d3c792ba79e224c0ef4bf8cb5d1d
Eb1n-Babu <ebin.babu.in@icloud.com>
2025-08-12 13:57:15 +0530

    updated backend/urls.py todos/urls.py

--- backend/todos/urls.py
+++ backend/todos/urls.py
@@ -1,2 +1,7 @@
 from django.urls import path
 from .views import TaskDetail,TaskViewSet
+
+urlpatterns = [
+    path('todos/',TaskViewSet.as_view(),name='todos-list'),
+    path('todos/<int:pk>/', TodoDetail.as_view(), name='todo-detail'),
+]

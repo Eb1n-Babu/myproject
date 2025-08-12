97dc74f035f5245a5c62cf6110f28d786ab26d03
Eb1n-Babu <ebin.babu.in@icloud.com>
2025-08-12 14:40:04 +0530

    modified todos/urls.py and made migrations

--- backend/todos/urls.py
+++ backend/todos/urls.py
@@ -1,7 +1,8 @@
+
 from django.urls import path
-from .views import TaskDetail,TaskViewSet
+from .views import TaskViewSet, TaskDetail
 
 urlpatterns = [
-    path('todos/',TaskViewSet.as_view(),name='todos-list'),
-    path('todos/<int:pk>/', TodoDetail.as_view(), name='todo-detail'),
+    path('todos/', TaskViewSet.as_view({'get': 'list', 'post': 'create'}), name='todos-list'),
+    path('todos/<int:pk>/', TaskDetail.as_view(), name='todo-detail'),
 ]

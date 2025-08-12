3c35079999a2f81df66accf77103f9a5f866db42
Eb1n-Babu <ebin.babu.in@icloud.com>
2025-08-12 15:47:53 +0530

    updated  src/App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [todos, setTodos] = useState([]);
+    const [newTodo, setNewTodo] = useState('');
+
+    useEffect(() => {
+        axios.get('http://localhost:8000/api/todos/')
+            .then(response => setTodos(response.data))
+            .catch(error => console.error(error));
+    }, []);
+
+    const addTodo = () => {
+        axios.post('http://localhost:8000/api/todos/', { title: newTodo, completed: false })
+            .then(response => setTodos([...todos, response.data]))
+            .catch(error => console.error(error));
+        setNewTodo('');
+    };
+
+    return (
+        <div>
+            <h1>Todo List</h1>
+            <input value={newTodo} onChange={e => setNewTodo(e.target.value)} />
+            <button onClick={addTodo}>Add Todo</button>
+            <ul>
+                {todos.map(todo => (
+                    <li key={todo.id}>{todo.title} - {todo.completed ? 'Done' : 'Pending'}</li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
